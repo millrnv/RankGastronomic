e0dc5c7c17e163fc8c1787258db09cbc33b2769d
NichoUlloa <n.ulloa06@ufromail.cl>
2023-11-20 16:13:46 -0300

    se agregan atributos a restorante

--- src/modelo/Restaurante.java
+++ src/modelo/Restaurante.java
@@ -1,4 +1,24 @@
 package modelo;
 
 public class Restaurante {
+    private String nombreRestaurante;
+    private Comida comida;
+
+    public Restaurante(String nombreRestaurante, Comida comida){
+        this.nombreRestaurante = nombreRestaurante;
+        this.comida = comida;
+    }
+
+    public void setNombreRestaurante(String nombreRestaurante){
+        this.nombreRestaurante = nombreRestaurante;
+    }
+
+    public void setComida(Comida comida){
+        this.comida = comida;
+    }
+
+    public String getNombreRestaurante(){
+        return nombreRestaurante;
+    }
+
 }
