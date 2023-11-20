d85d2f930a65929e2017ec34ffef274c556a6d31
NichoUlloa <n.ulloa06@ufromail.cl>
2023-11-20 16:45:37 -0300

    se agregan atributos y metodos a restorante, Usuario, Reseña y RankGastronomic

--- src/modelo/RankGastronomic.java
+++ src/modelo/RankGastronomic.java
@@ -5,18 +5,49 @@ import java.util.List;
 public class RankGastronomic {
 
     private List<Restaurante> restaurantes;
+    private List<Usuario> usuarios;
+    private List<Reseña> reseñas;
+    private List<Comida> comidas;
 
-    public RankGastronomic(List<Restaurante> restaurantes){
+    public RankGastronomic(List<Restaurante> restaurantes, List<Usuario> usuarios, List<Reseña> reseñas, List<Comida> comidas){
         this.restaurantes = restaurantes;
+        this.usuarios = usuarios;
+        this.reseñas = reseñas;
+        this.comidas = comidas;
     }
 
     public void setRestaurantes(List<Restaurante> restaurantes){
         this.restaurantes = restaurantes;
     }
 
+    public void setUsuarios(List<Usuario> usuarios){
+        this.usuarios = usuarios;
+    }
+
+    public void setReseñas(List<Reseña> reseñas){
+        this.reseñas = reseñas;
+    }
+
+    public void setComidas(List<Comida> comidas){
+        this.comidas = comidas;
+    }
+
     public List<Restaurante> getRestaurantes(){
         return restaurantes;
     }
+
+    public List<Usuario> getUsuarios(){
+        return usuarios;
+    }
+
+    public List<Reseña> getReseñas(){
+        return reseñas;
+    }
+
+    public List<Comida> getComidas(){
+        return comidas;
+    }
+
 
     public void generarMenu() {
         // TODO - implement modelo.RankGastronomic.generarMenu
