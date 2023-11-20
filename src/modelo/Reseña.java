c3fb5b0b5e7595a9ea4ec8dd20fa22609dc00cd0
azambrao02 <a.zambrano02@ufromail.cl>
2023-11-20 00:11:27 -0300

    cree el package modelo donde movi las clases ya creadas y le di atributos a la clase comida y a la clase reseña

--- src/modelo/Reseña.java
+++ src/modelo/Reseña.java
@@ -1,4 +1,15 @@
 package modelo;
 
 public class Reseña {
+    private String opinion;
+
+    public Reseña (String opinion){
+        this.opinion = opinion;
+    }
+    public void setOpinion(String opinion) {
+        this.opinion = opinion;
+    }
+    public String getOpinion(){
+        return opinion;
+    }
 }
